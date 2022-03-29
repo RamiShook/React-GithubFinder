d3ed3fd1fae24356ee2daa93e7013ee1c9f55099
BIGRamiShook <rami.shook@blockchaingroup.io>
2022-03-29 22:16:55 +0300

    Basic Pages

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GithubFinder</h1>
+      <p>
+        {/* @todo fill this  */}
+        Some information about this project
+      </p>
+    </div>
+  );
+}
+
+export default About;
